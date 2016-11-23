6e1ba351e12271a7737121b111fc9194406ddc6c
serversteam <365.orion@gmail.com>
2016-11-23 12:39:43 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel-test1</title>
+        <title>Laravel-test2</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-test1
+                    Laravel-test2
                 </div>
 
                 <div class="links">
